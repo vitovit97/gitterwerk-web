d88c5c434a50a15b931c3dd1297e91b42771aa0f
vitovit97 <kovito1997@gmail.com>
2024-10-06 19:46:28 +0200

    Delete js directory

--- js/search-bar.js
+++
@@ -1,8 +0,0 @@
-const div = document.getElementById('Search-Button');
-
-// Add a click event listener to the button
-div.addEventListener('click', function() {
-    // Toggle the "active" class on the button
-    div.classList.toggle('active');
-});
-
